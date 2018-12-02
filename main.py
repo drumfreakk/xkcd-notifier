06e3ab8a642c334b18d8c896c8f9eefe1663d8af
Kieran H <kieran.houtgraaf@gmail.com>
2018-12-02 11:33:26 +0100

    Improved error handling

--- main.py
+++ main.py
@@ -4,64 +4,82 @@ from time import sleep, strftime
 import smtplib
 from email.message import EmailMessage
 
-os.chdir('/home/pi/xkcd-notifier')
+os.chdir('/home/kip/Desktop/t/xkcd-notifier')
 
 what = 1990
 times = 0
 done = 0
 
 def useFile(doc, manner, write):
-    manners = ['r', 'a', 'w', 'w+']
-    if manner in manners:
-        f = open(doc, manner)
-        if manner == 'r':
-            doc = f.read()
-            f.close()
-            return doc
-        else:
-            f.write(write)
-            f.close()
-    else:
-        return "Invalid manner"
+	manners = ['r', 'a', 'w', 'w+']
+	if manner in manners:
+		f = open(doc, manner)
+		if manner == 'r':
+			doc = f.read()
+			f.close()
+			return doc
+		else:
+			f.write(write)
+			f.close()
+	else:
+		return "Invalid manner"
 
-def sendMail(toaddr, subject, body):
-    msg = EmailMessage()
-    msg.set_content(body)
+try:
 
-    msg['Subject'] = subject
-    msg['From'] = 'xkcdnotifications@gmail.com'
-    msg['To'] = 'kieran.houtgraaf@gmail.com'
+	def sendMail(toaddr, subject, body):
+		msg = EmailMessage()
+		msg.set_content(body)
 
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login('xkcdnotifications@gmail.com', "LekkereKippenBoutjes!")
-    s.send_message(msg)
-    s.quit()
+		msg['Subject'] = subject
+		msg['From'] = 'xkcdnotifications@gmail.com'
+		msg['To'] = 'kieran.houtgraaf@gmail.com'
 
-def test():
-    r = requests.get('https://www.xkcd.com/' + str(what) + '/')
-    return r.status_code
+		s = smtplib.SMTP('smtp.gmail.com', 587)
+		s.starttls()
+		s.login('xkcdnotifications@gmail.com', "LekkereKippenBoutjes!")
+		s.send_message(msg)
+		s.quit()
 
-what = int(useFile('latest', 'r', '')[0:4])
-while True:
-    status = test()
-    print(str(strftime('%Y-%m-%d %H:%M:%S')) + ' XKCD no.: ' + str(what) + ' Status: ' + str(status))
-    if status == 200:
-        what += 1
-        if times == 0:
-            useFile('latest', 'w', str(what) + 'False')
-            times = 1
-        else:
-            done += 1
-    elif status == 404:
-        if useFile('latest', 'r', '')[4:] == 'False':
-            if done == 0:
-                sendMail('kieran.houtgraaf@gmail.com', 'There is a new xkcd comic! We are up to ' + str(what - 1) + '!', 'https://www.xkcd.com/' + str(what - 1))
-            else:
-                sendMail('kieran.houtgraaf@gmail.com', 'There is a new xkcd comic! We are up to ' + str(what - 1) + '!', 'https://www.xkcd.com/' + str(what - 1) + '\n(And ' + str(done) + ' more...)')
-        useFile('latest', 'w', str(what) + 'True')
-        done = 0
-        quit()
-    else:
-        sendMail('kieran.houtgraaf@gmail.com', 'Error?', 'Non-200 or 404 status code: ' + str(status))
-        quit()
+	def test():
+		r = requests.get('https://www.xkcd.com/' + str(what) + '/')
+		return r.status_code
+
+	what = int(useFile('latest', 'r', '')[0:4])
+
+	while True:
+		status = test()
+		print(str(strftime('%Y-%m-%d %H:%M:%S')) + ' XKCD no.: ' + str(what) + ' Status: ' + str(status))
+
+		if useFile('latest', 'r', '')[4:] == 'Internet':
+			sendMail('kieran.houtgraaf@gmail.com', 'Internet Error', 'On one ore more runs there was no internet connection, causing it to error. Check the logs for more details.')
+			print(str(strftime('%Y-%m-%d %H:%M:%S')) + ' Internet error resolved')
+
+		if status == 200:
+			what += 1
+			if times == 0:
+				useFile('latest', 'w', str(what) + 'False')
+				times = 1
+
+			else:
+				done += 1
+
+		elif status == 404:
+			if useFile('latest', 'r', '')[4:] == 'False':
+				if done == 0:
+					sendMail('kieran.houtgraaf@gmail.com', 'There is a new xkcd comic! We are up to ' + str(what - 1) + '!', 'https://www.xkcd.com/' + str(what - 1))	
+
+				else:
+					sendMail('kieran.houtgraaf@gmail.com', 'There is a new xkcd comic! We are up to ' + str(what - 1) + '!', 'https://www.xkcd.com/' + str(what - 1) + '\n(And ' + str(done) + ' more...)')
+
+			useFile('latest', 'w', str(what) + 'True')
+			done = 0
+			quit()
+
+		else:
+			sendMail('kieran.houtgraaf@gmail.com', 'Error?', 'Non-200 or 404 status code: ' + str(status))
+			quit()
+
+except requests.exceptions.ConnectionError:
+	print(str(strftime('%Y-%m-%d %H:%M:%S')) + ' Failed due to no internet connection.')
+	useFile('latest', 'w', str(what) + 'Internet')
+
